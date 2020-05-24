1f3f93afc0ebfc661687622ce0dbcc6e4ba55d55
mystfoo <1771274574@qq.com>
2020-05-24 11:24:12 +0800

    init

---
+++ src/main/java/com/mystfoo/weeds/WeedsApplication.java
@@ -0,0 +1,13 @@
+package com.mystfoo.weeds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeedsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeedsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mystfoo/weeds/WeedsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mystfoo.weeds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeedsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
